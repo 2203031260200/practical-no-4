f66434a18f36bd91407f54b01cc548d150666be8
2203031260200 <157494138+2203031260200@users.noreply.github.com>
2024-01-24 00:03:55 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,5 +1,7 @@
+fh=open("output 4.txt",'w')
 def average(a);
 return sum(a)/len(a)
 a=[1,3,5,7,9]
 average=average(a)
-print("the average of given list is:",average)
+fh.write("the average of given list is:"+str(average))
+fh.close
